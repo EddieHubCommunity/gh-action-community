737761afe6c26f9f20cfd9cbce5d64472ad9a1c4
Eddie Jaoude <eddie@jaoudestudios.com>
2020-09-05 08:21:25 +0200

    fix(action): context parameters #1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const github = require('@actions/github');
     // const githubSecret = core.getInput('github-secret');
 
     const creator = github.context.payload.sender.login;
-    const opts = github.context.repo.repo.issues.listForRepo.endpoint.merge({
+    const opts = github.context.repo.issues.listForRepo.endpoint.merge({
       ...github.context.issue,
       creator,
       state: 'all',
