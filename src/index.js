4784248bf26e6538ff77167471bb1abc67fa03d3
Eddie Jaoude <eddie@jaoudestudios.com>
2020-09-05 11:44:42 +0200

    fix(stats): firestore doc path #18

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ const admin = require('firebase-admin');
       credential: admin.credential.cert(JSON.parse(firebaseKey)),
     });
 
-    // const db = admin.firestore();
-    // const author = github.context.payload.sender;
+    const db = admin.firestore();
+    const author = github.context.payload.sender;
     const type = process.env.GITHUB_EVENT_NAME;
-console.log(type, github.context.payload);
-    // const docRef = db.collection('usersGitHub').doc(author.id);
-    // await docRef.set({
-    //   ...author,
-    //   type: admin.firestore.FieldValue.increment(1)
-    // }, { merge: true });
+
+    const docRef = db.collection('usersGitHub').doc(author.id.toString());
+    await docRef.set({
+      ...author,
+      type: admin.firestore.FieldValue.increment(1)
+    }, { merge: true });
   } catch (error) {
     core.setFailed(error.message);
   }
