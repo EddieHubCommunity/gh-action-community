74a0ab5a77618500b7109707749a7fccbab8adde
rishsinghal <zzxy334@gmail.com>
2021-02-05 23:13:04 +0530

    fixed broken link

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -17,7 +17,7 @@ const github = require('@actions/github');
       return;
     }
 
-    const footer = `<p>If you would like to continue contributing to open source and would like to do it with an awesome inclusive community, you should join our <a href="https://discord.com/invite/jZQs6Wu">Discord</a> chat and our <a href="https://github.com/EddieJaoudeCommunity">GitHub Organisation</a> - we help and encourage each other to contribute to open source little and often 🤓 . Any questions let us know.</p>
+    const footer = `<p>If you would like to continue contributing to open source and would like to do it with an awesome inclusive community, you should join our <a href="https://discord.com/invite/jZQs6Wu">Discord</a> chat and our <a href="https://github.com/EddieHubCommunity">GitHub Organisation</a> - we help and encourage each other to contribute to open source little and often 🤓 . Any questions let us know.</p>
       `;
 
     if (!!context.payload.issue) {
