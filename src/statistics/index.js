3befb0ea972a4c4570e51d36a3a79d6244ce9006
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-29 09:11:10 +0000

    feat: update statistics action

--- src/statistics/index.js
+++ src/statistics/index.js
@@ -1,29 +1,27 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const admin = require('firebase-admin');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios").default;
 
 (async () => {
   try {
-    const firebaseKey = core.getInput('firebase-key', { required: true });
+    const apiKey = core.getInput("api-key", { required: true });
+    const apiURL = core.getInput("api-url", { required: true });
 
+    const authHeader = { "Client-Token": apiKey };
     // save statistics to db
-    admin.initializeApp({
-      credential: admin.credential.cert(JSON.parse(firebaseKey)),
-    });
 
-    const db = admin.firestore();
     const author = github.context.payload.sender;
     const type = process.env.GITHUB_EVENT_NAME;
 
-    const docRef = db.collection('usersGitHub').doc(author.id.toString());
-    await docRef.set(
-      {
-        author,
-        id: author.id.toString(),
-        [type]: admin.firestore.FieldValue.increment(1),
-      },
-      { merge: true }
-    );
+    const body = {
+      githubUsername: author,
+      event: type,
+    };
+    try {
+      await axios.post(apiURL, body, {
+        headers: { ...authHeader },
+      });
+    } catch (e) {}
   } catch (error) {
     core.setFailed(error.message);
   }
