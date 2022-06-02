f003a23e89b0d69bd1e8df68fe4c34844b977263
Eddie Jaoude <eddie@jaoudestudios.com>
2022-06-02 15:53:39 +0100

    fix: only run on label event

--- src/api-user/index.js
+++ src/api-user/index.js
@@ -6,19 +6,40 @@ const axios = require("axios").default;
   try {
     const apiKey = core.getInput("api-key", { required: true });
     const apiURL = core.getInput("api-url", { required: true });
+    const repoToken = core.getInput('repo-token', { required: true });
+    const client = github.getOctokit(repoToken);
+    const currentLabel = payload.label.name;
 
     const authHeader = { "Client-Token": apiKey };
     const author = github.context.payload.sender;
     const body = {
       githubUsername: author.login,
     };
-    try {
-      console.log(body);
-      await axios.post(apiURL, body, {
-        headers: { ...authHeader },
-      });
-    } catch (e) {
-      console.log(e.response.data)
+
+    
+    if (currentLabel === 'EddieHub-API-add') {
+      try {
+        console.log(body);
+        await axios.post(apiURL, body, {
+          headers: { ...authHeader },
+        });
+        
+        await client.issues.createComment({
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          issue_number: payload.issue.number,
+          body: 'You have been ADDED to the EddieHub API',
+        });
+
+        await client.issues.update({
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          issue_number: payload.issue.number,
+          state: 'closed',
+        });
+      } catch (e) {
+        console.log(e.response.data)
+      }
     }
   } catch (error) {
     core.setFailed(error.message);
